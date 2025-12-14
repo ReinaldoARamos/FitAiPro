fc1cdcc4492bfca429aec9e4684c108960facc35
Reinaldo <Gamersolitavi4l@gmail.com>
2025-12-14 16:14:46 -0300

    feature: criando a rota para criar os treinos

--- app/api/workout/route.ts
+++ app/api/workout/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const {userId, treinoA, treinoB, treinoC } = body;
+    const { userId, treinoA, treinoB, treinoC } = body;
 
     if (!userId || !treinoA || !treinoB || !treinoC) {
       return NextResponse.json(
@@ -16,16 +16,49 @@ export async function POST(req: Request) {
         { status: 500 }
       );
     }
-
-    const plan  = await prisma.workoutPlan.create({
+    //funcao que eu criei para gerar o plano ja relacionando o user
+    const plan = await prisma.workoutPlan.create({
       data: {
-      userId: Number(userId)
-      }
-    })
+        userId: Number(userId),
+      },
+    });
+
+    //criacao dos exercicios
+    const createExercises = (exlist: any[], workoutDayId: number) => {
+      Promise.all(
+        exlist.map((ex) => {
+          prisma.exercise.create({
+            data: {
+              exercise: ex.exercise,
+              reps: ex.reps,
+              restTime: ex.restTime,
+              howtoExecute: ex.howtoExecute,
+              workoutDayId,
+            },
+          });
+        })
+      );
+    };
+
+    const treinoAday = await prisma.workoutDay.create({
+      data: { label: "TreinoA", planId: plan.id },
+    });
+    const treinoBday = await prisma.workoutDay.create({
+      data: { label: "TreinoB", planId: plan.id },
+    });
+    const treinoCday = await prisma.workoutDay.create({
+      data: { label: "TreinoC", planId: plan.id },
+    });
+
+    await createExercises(treinoA, treinoAday.id);
+    await createExercises(treinoB, treinoBday.id);
+    await createExercises(treinoC, treinoCday.id);
+
+    return NextResponse.json({ sucess: true, planId: plan.id });
   } catch (error) {
     return NextResponse.json(
       { error: "erro ao chegar a ficha" },
-      { status: 400 }
+      { status: 500 }
     );
   }
 }
